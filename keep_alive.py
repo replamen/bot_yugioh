f79afd1385bb9ee3136d8081e5c40dc84f61aef0
Shaunce CARTE <carte.shaunce2005@gmail.Com>
2024-11-20 23:55:55 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-from discord import threads
 from flask import Flask
 from threading import Thread
 
@@ -6,11 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is online!"
+    return "Le bot est en ligne!"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    t = Thread(target=run)
+    t.start()
